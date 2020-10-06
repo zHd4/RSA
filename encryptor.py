f5b0f4de30f57102dabbcb652a2319ebaa4d8868
zhd4 <computersystembase@gmail.com>
2020-10-06 14:07:34 +0300

    encryptor.py Changed to pem2key convertation Created blank split and join to blocks methods

--- encryptor.py
+++ encryptor.py
@@ -1,12 +1,19 @@
+from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 
 class Encryptor:
     def __init__(self, key_length, public_key):
         self.__length = key_length
-        self.__public_key = public_key
+        self.__public_key = RSA.importKey(public_key)
 
-        self.__crypto = PKCS1_OAEP.new(public_key)
+        self.__crypto = PKCS1_OAEP.new(self.__public_key)
+
+    def __split_to_blocks(self):
+        pass
+
+    def __join_to_blocks(self):
+        pass
 
     def encrypt(self, data_bytes):
         return self.__crypto.encrypt(data_bytes)
