13d1883832ee95e732a8afe01fb1f328b7e5e22f
zhd4 <computersystembase@gmail.com>
2020-10-06 22:44:39 +0300

    Changed methods order in encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -9,6 +9,9 @@ class Encryptor:
 
         self.__crypto = PKCS1_OAEP.new(self.__public_key)
 
+    def encrypt(self, data_bytes):
+        return self.__crypto.encrypt(data_bytes)
+
     def __split_to_blocks(self, data):
         size, length = len(data), self.__length
 
@@ -33,6 +36,3 @@ class Encryptor:
             return b''.join(blocks)
         else:
             return blocks
-
-    def encrypt(self, data_bytes):
-        return self.__crypto.encrypt(data_bytes)
