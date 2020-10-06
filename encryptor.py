ccf6c0726126406456df4545b56e2a19d86c39a2
zHd4 <computersystembase@gmail.com>
2020-10-06 21:42:27 +0300

    Implemented split_to_blocks() in encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -9,10 +9,26 @@ class Encryptor:
 
         self.__crypto = PKCS1_OAEP.new(self.__public_key)
 
-    def __split_to_blocks(self):
-        pass
+    def __split_to_blocks(self, data):
+        size = len(data)
+        length = self.__length
 
-    def __join_to_blocks(self):
+        if size <= length:
+            return [data]
+        else:
+            result = []
+
+            for i in range(int(size / length)):
+                result.append(data[length * i:length * (i + 1)])
+
+            last = size % length
+
+            if last != 0:
+                result.append(data[:size - last])
+
+            return result
+
+    def __join_blocks(self):
         pass
 
     def encrypt(self, data_bytes):
