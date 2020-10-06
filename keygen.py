7929da95ef2c2a33e9a0fd50047424b2ec2221cf
zhd4 <computersystembase@gmail.com>
2020-10-06 13:08:01 +0300

    Implemented keygen.py

---
+++ keygen.py
@@ -0,0 +1,12 @@
+from Crypto.PublicKey import RSA
+
+
+class KeyGen:
+    def __init__(self, key_length):
+        self.__key_pair = RSA.generate(key_length)
+
+    def get_public_key(self):
+        return self.__key_pair.publickey().exportKey()
+
+    def get_private_key(self):
+        return self.__key_pair.exportKey()
