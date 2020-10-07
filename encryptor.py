608534022ec074eaaae11db21891b0441e85cdf1
zhd4 <computersystembase@gmail.com>
2020-10-07 12:16:54 +0300

    Removed public_key private field from encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -5,9 +5,7 @@ from Crypto.Cipher import PKCS1_OAEP
 class Encryptor:
     def __init__(self, key_length, public_key):
         self.__length = key_length
-        self.__public_key = RSA.importKey(public_key)
-
-        self.__crypto = PKCS1_OAEP.new(self.__public_key)
+        self.__crypto = PKCS1_OAEP.new(public_key)
 
     def encrypt(self, data_bytes):
         encrypted_chunks = []
