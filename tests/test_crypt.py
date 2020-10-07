91fc205ae8c067a012b904e059930f4ed1b177c2
zhd4 <computersystembase@gmail.com>
2020-10-07 14:34:32 +0300

    Implemented test_keygen() method in test_crypt.py

--- tests/test_crypt.py
+++ tests/test_crypt.py
@@ -1,8 +1,15 @@
 import unittest
+from keygen import KeyGen
 
 
 class TestCrypt(unittest.TestCase):
-    pass
+    def test_keygen(self):
+        self.__key_size = 4096
+
+        keygen = KeyGen(self.__key_size)
+
+        self.__public_key = keygen.get_public_key()
+        self.__private_key = keygen.get_private_key()
 
 
 if __name__ == '__main__':
