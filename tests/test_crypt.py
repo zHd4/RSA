d56def3c829d23bd9d07da95ba17e12d3474f5c0
zhd4 <computersystembase@gmail.com>
2020-10-07 18:26:14 +0300

    All test logic moved to test_crypt() in test_crypt.py

--- tests/test_crypt.py
+++ tests/test_crypt.py
@@ -1,32 +1,27 @@
 import unittest
-
-from decryptor import Decryptor
-from encryptor import Encryptor
 from keygen import KeyGen
+from encryptor import Encryptor
+from decryptor import Decryptor
 
 
 class TestCrypt(unittest.TestCase):
-    def test_keygen(self):
-        self.__key_size = 4096
-
+    def test_crypt(self):
+        key_size = 4096
         test_file = '/bin/bash'
 
         with open(test_file, 'rb') as bin_bash:
-            self.__test_data = bin_bash.read()
+            test_data = bin_bash.read()
 
-        keygen = KeyGen(self.__key_size)
+        keygen = KeyGen(key_size)
 
-        self.__public_key = keygen.get_public_key()
-        self.__private_key = keygen.get_private_key()
+        public_key = keygen.get_public_key()
+        private_key = keygen.get_private_key()
 
-    def test_encryption(self):
-        self.__encrypted = Encryptor(self.__key_size, self.__public_key).encrypt(self.__test_data)
+        encrypted = Encryptor(key_size, public_key).encrypt(test_data)
 
-    def test_decryption(self):
-        self.__decrypted = Decryptor(self.__key_size, self.__private_key).decrypt(self.__encrypted)
+        decrypted = Decryptor(key_size, private_key).decrypt(encrypted)
 
-    def check_tests(self):
-        self.assertEqual(self.__test_data, self.__decrypted, msg='Initial and decrypted data are not equal')
+        self.assertEqual(test_data, decrypted, msg='Initial and decrypted data are not equal')
 
 
 if __name__ == '__main__':
