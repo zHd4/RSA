338be7323934e769a5d99ed446d071813bfb9702
zhd4 <computersystembase@gmail.com>
2020-10-07 12:15:50 +0300

    Created basic structure of decryptor.py

---
+++ decryptor.py
@@ -0,0 +1,18 @@
+from Crypto.Cipher import PKCS1_OAEP
+
+
+class Decryptor:
+    def __init__(self, key_length, private_key):
+        self.__length = key_length
+        self.__crypto = PKCS1_OAEP.new(private_key)
+
+    def decrypt(self, data_bytes):
+        pass
+
+    def __split_to_blocks(self, data):
+        size, length = len(data), self.__length
+        return [data[i:i + length] for i in range(0, size, length)]
+
+    # noinspection PyMethodMayBeStatic
+    def __join_blocks(self, blocks):
+        return b''.join(blocks) if len(blocks) > 0 else blocks
